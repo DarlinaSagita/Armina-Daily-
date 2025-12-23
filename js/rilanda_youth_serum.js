2ebe112d50736e264d0c74e647e241a7956365a8
unknown <nobelleon.86@gmail.com>
2025-12-23 20:08:58 +0700

    adding rilanda youth serum

---
+++ js/rilanda_youth_serum.js
@@ -0,0 +1,176 @@
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const scrollReveal = function(){
+  for(let i = 0; i < revealElements.length; i++){
+    const isElementsOnScreen = revealElements[i].getBoundingClientRect().top < window.innerHeight;
+    
+    if(isElementsOnScreen){
+      revealElements[i].classList.add("revealed")
+    } else {
+      revealElements[i].classList.remove("revealed")
+    }
+  }
+}
+
+window.addEventListener("scroll", scrollReveal);
+window.addEventListener("load", scrollReveal);
+
+
+// -------------------------------  Floating Menu  ----------------------------- //
+//------------------------------------------------------------------------------ //
+
+// Get all menu from document
+document.querySelectorAll('.fabTrigger').forEach(OpenMenu);
+
+// Menu Open and Close function
+function OpenMenu(active) {
+  if(active.classList.contains('fabTrigger') === true){
+    active.addEventListener('click', function (e) {
+      e.preventDefault();        
+
+      if (this.parentElement.classList.contains('active') === true) {
+        // Close the clicked dropdown
+        this.parentElement.classList.remove('active');
+
+      } else {
+        // Close the opend dropdown
+        closeMenu();
+        // add the open and active class(Opening the DropDown)
+        this.parentElement.classList.add('active');
+      }
+    });
+  }
+};
+
+// Close the openend Menu
+function closeMenu() { 
+  // remove the open and active class from other opened Moenu (Closing the opend Menu)
+  document.querySelectorAll('.fab').forEach(function (container) { 
+    container.classList.remove('active')
+  });
+}
+
+// ------------------------------------------------------------------- //
+// -----------------------  CUSTOM DOT CURSOR  ----------------------- //
+
+var cursor = {
+    delay: 8,
+    _x: 0,
+    _y: 0,
+    endX: (window.innerWidth / 2),
+    endY: (window.innerHeight / 2),
+    cursorVisible: true,
+    cursorEnlarged: false,
+    $dot: document.querySelector('.cursor-dot'),
+    $outline: document.querySelector('.cursor-dot-outline'),
+    
+    init: function() {
+        // Set up element sizes
+        this.dotSize = this.$dot.offsetWidth;
+        this.outlineSize = this.$outline.offsetWidth;
+        
+        this.setupEventListeners();
+        this.animateDotOutline();
+    },
+    
+    setupEventListeners: function() {
+        var self = this;
+        
+        // Anchor hovering
+        document.querySelectorAll('a').forEach(function(el) {
+            el.addEventListener('mouseover', function() {
+                self.cursorEnlarged = true;
+                self.toggleCursorSize();
+            });
+            el.addEventListener('mouseout', function() {
+                self.cursorEnlarged = false;
+                self.toggleCursorSize();
+            });
+        });
+        
+        // Click events
+        document.addEventListener('mousedown', function() {
+            self.cursorEnlarged = true;
+            self.toggleCursorSize();
+        });
+        document.addEventListener('mouseup', function() {
+            self.cursorEnlarged = false;
+            self.toggleCursorSize();
+        });
+  
+  
+        document.addEventListener('mousemove', function(e) {
+            // Show the cursor
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+
+            // Position the dot
+            self.endX = e.pageX;
+            self.endY = e.pageY;
+            self.$dot.style.top = self.endY + 'px';
+            self.$dot.style.left = self.endX + 'px';
+        });
+        
+        // Hide/show cursor
+        document.addEventListener('mouseenter', function(e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+            self.$dot.style.opacity = 1;
+            self.$outline.style.opacity = 1;
+        });
+        
+        document.addEventListener('mouseleave', function(e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+            self.$dot.style.opacity = 0;
+            self.$outline.style.opacity = 0;
+        });
+    },
+    
+    animateDotOutline: function() {
+        var self = this;
+        
+        self._x += (self.endX - self._x) / self.delay;
+        self._y += (self.endY - self._y) / self.delay;
+        self.$outline.style.top = self._y + 'px';
+        self.$outline.style.left = self._x + 'px';
+        
+        requestAnimationFrame(this.animateDotOutline.bind(self));
+    },
+    
+    toggleCursorSize: function() {
+        var self = this;
+        
+        if (self.cursorEnlarged) {
+            self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)';
+            self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
+        } else {
+            self.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
+            self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
+        }
+    },
+    
+    toggleCursorVisibility: function() {
+        var self = this;
+        
+        if (self.cursorVisible) {
+            self.$dot.style.opacity = 1;
+            self.$outline.style.opacity = 1;
+        } else {
+            self.$dot.style.opacity = 0;
+            self.$outline.style.opacity = 0;
+        }
+    }
+}
+
+cursor.init();
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
